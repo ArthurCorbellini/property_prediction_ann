ca86c7b89db774de171fdaa9fd7bbeb33338cda4
Arthur Corbellini <arthur.corbellini27@gmail.com>
2023-09-18 21:42:47 -0300

    remoção de versões antigas

--- v2/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import feature_eng as feat_eng
-
-pd.set_option('display.max_columns', None)
-data = pd.read_csv('datasets/sao-paulo-properties-april-2019.csv')
-
-for col in data.columns:
-    # print(col, '-', data[col].nunique())
-    feat_eng.diagnostic_plots(data, col)
